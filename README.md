49accb70b5fd7a7a4723052b1738f82d618f39c4
Ross Golder <ross@golder.org>
2018-08-28 12:28:50 +0700

    Drop build/push bits.

--- README.md
+++ README.md
@@ -8,21 +8,16 @@ Requires the following environment variables to be made available:
 
 Env var | Description | Example
 --------|-------------|--------
-WPCD_DOCKER_IMAGE | The registry URL to push the container to and to tell K8S to pull it from | registry.gitlab.com/myorganisation/myproject:latest
+IMAGE_URI | The registry URL to push the container to and to tell K8S to pull it from | registry.gitlab.com/myorganisation/myproject:latest
 K8S_ENDPOINT_URL | The base URL of the Kubernetes API service | https://k8s.in.yourdomain.com:6443
 K8S_TOKEN | The token to use for the 'Authorization' header |
 K8S_NAMESPACE | The namespace in which to find the deployment |
 K8S_DEPLOYMENT | The name of the deployment which is to be upgraded |
 K8S_CONTAINER | The name of the container to be upgraded |
 
-It first invokes 'docker build' to build the image, and 'docker push' to push it to the registry.
+This driver simply patches the image name for the container via the Kubernetes API, which should trigger a rolling upgrade on the deployment.
 
-Then, it then patches the image name for the container via the Kubernetes API, which should trigger a rolling upgrade on the deployment.
+NOTE: If the IMAGE_URI is not tagged as 'latest' and doesn't change between deployments, Kubernetes will not upgrade the containers.
 
-In order for a deployment to update, the image name must change between deployments. Therefore, you can use a dynamic tag in the 'WPCD_DOCKER_IMAGE' variable:
+TIP: Use a variable related to your CI pipeline as the tag in IMAGE_URI.
 
-```
-export WPCD_DOCKER_IMAGE=registry.yourdomain.com/path/yourapplication:{TS}
-```
-
-The above replaces the image tag with a timestamp in the format 'YYYYMMDDHHMMSS'. In addition to `{TS}`, there is `{UID}` which will insert a random 6-char string.
--- wordpress_cd_k8s/k8s.py
+++ wordpress_cd_k8s/k8s.py
@@ -24,70 +24,29 @@ class KubernetesDriver(BaseDriver):
         _logger.debug("Initialising Kubernetes Deployment Driver")
 
         try:
-            self.image_uri = os.environ['WPCD_DOCKER_IMAGE']
             self.k8s_endpoint_url = os.environ['K8S_ENDPOINT_URL']
+            self.k8s_token = os.environ['K8S_TOKEN']
         except KeyError as e:
             raise Exception("Missing '{0}' environment variable.".format(e))
 
-        ts = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        self.image_uri = self.image_uri.replace("{uid}", randomword(6))
-        self.image_uri = self.image_uri.replace("{ts}", ts)
         self.mysql_root_pass = randomword(10)
 
-    def _get_docker_compose_yml(self, docker_image):
-        with open("config/k8s/docker-compose.yml") as f:
-            docker_compose_yml = f.read()
-        docker_compose_yml = docker_compose_yml.replace("{{ docker_image }}", docker_image)
-        docker_compose_yml = docker_compose_yml.replace("{{ mysql_root_pass }}", self.mysql_root_pass)
-        docker_compose_yml = docker_compose_yml.replace("{{ mysql_user }}", self.test_dataset.mysql_user)
-        docker_compose_yml = docker_compose_yml.replace("{{ mysql_pass }}", self.test_dataset.mysql_pass)
-        docker_compose_yml = docker_compose_yml.replace("{{ mysql_db }}", self.test_dataset.mysql_db)
-        return docker_compose_yml
-
-    def _get_k8s_compose_yml(self):
-        with open("config/k8s/k8s-compose.yml") as f:
-            k8s_compose_yml = f.read()
-        k8s_compose_yml = k8s_compose_yml.replace("{{ hostname }}", self.test_site_fqdn)
-        return k8s_compose_yml
-
-    def _docker_build(self):
-        buildargs = ['docker', 'build', '-t', self.image_uri, '.']
-        buildenv = os.environ.copy()
-        buildproc = subprocess.Popen(buildargs, stderr=subprocess.PIPE, env=buildenv)
-        buildproc.wait()
-        exitcode = buildproc.returncode
-        errmsg = buildproc.stderr.read()
-        if exitcode != 0:
-            raise Exception("Error while building image: %s" % errmsg)
-
-    def _docker_push(self):
-        pushargs = ['docker', 'push', self.image_uri]
-        pushenv = os.environ.copy()
-        pushproc = subprocess.Popen(pushargs, stderr=subprocess.PIPE, env=pushenv)
-        pushproc.wait()
-        exitcode = pushproc.returncode
-        errmsg = pushproc.stderr.read()
-        if exitcode != 0:
-            raise Exception("Error while pushing image: %s" % errmsg)
-
-    def _upgrade_k8s_service(self, namespaceId, deploymentId):
+    def _upgrade_k8s_service(self, namespaceId, deploymentId, containerId, imageUri):
         # Use Kubernetes API call to update a deployment:
         # http://k8s-endpoint/apis/extensions/v1beta1/namespaces/<namespace>/deployments/<deployment>
 
         endpoint_url = "{0}/apis/apps/v1/namespaces/{1}/deployments/{2}".format(self.k8s_endpoint_url, namespaceId, deploymentId)
-        token = os.environ['K8S_TOKEN']
-        container_name = os.environ['K8S_CONTAINER']
         headers = {
             'Content-Type': 'application/strategic-merge-patch+json',
-            'Authorization': "Bearer " + token
+            'Authorization': "Bearer " + self.k8s_token
         }
         payload = {
             "spec": {
                 "template": {
                     "spec": {
                         "containers": [{
-                            "name": container_name,
-                            "image": self.image_uri
+                            "name": containerId,
+                            "image": imageUri
                         }]
                     }
                 }
@@ -95,7 +54,7 @@ class KubernetesDriver(BaseDriver):
         }
 
         # Upgrade the service with payload
-        _logger.info("Updating container '{}' to use image '{}'... ".format(container_name, self.image_uri))
+        _logger.info("Updating container '{}' to use image '{}'... ".format(containerId, imageUri))
         r = requests.patch(endpoint_url, data=json.dumps(payload), headers=headers)
         res = r.json()
         if r.status_code != 200:
@@ -103,17 +62,11 @@ class KubernetesDriver(BaseDriver):
 
     def deploy_site(self):
         _logger.info("Deploying site to Kubernetes environment (job id: {0})...".format(self.job_id))
-
-        #_logger.info("Building new docker image...")
-        self._docker_build()
-
-        #_logger.info("Pushing new docker image...")
-        self._docker_push()
-
-        _logger.info("Deploying new docker image to k8s service...")
-        namespaceId = os.environ['K8S_NAMESPACE']
-        deploymentId = os.environ['K8S_DEPLOYMENT']
-        self._upgrade_k8s_service(namespaceId, deploymentId)
+        namespace = os.environ['K8S_NAMESPACE']
+        deployment = os.environ['K8S_DEPLOYMENT']
+        container = os.environ['K8S_CONTAINER']
+        image_uri = os.environ['IMAGE_URI']
+        self._upgrade_k8s_service(namespace, deployment, container, image_uri)
 
         # Done
         _logger.info("Deployment successful.")
